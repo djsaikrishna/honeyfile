3cf285c7aa0401fbb96e0d70e96edc14b3f28cd5
K. M. Zakaria Alam <149001544+kmzakarialam@users.noreply.github.com>
2023-11-18 19:08:23 +0600

    Add files via upload

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '21980035'))
-    API_HASH = str(getenv('API_HASH', '667aba805ed82b19ec391bfaeafa69e4'))
+    API_ID = int(getenv('API_ID', '22752348'))
+    API_HASH = str(getenv('API_HASH', '5190a8e38585ba9a355f86a9dd9b38ce'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
     name = str(getenv('name', 'AnyFileToLinkBot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002045422729'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002136063437'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6483776439").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6642047024").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'MExAkib'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'THExAkib'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://FileToLink:FileToLink@cluster0.zraj6ks.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://FileToLink:FileToLink@cluster0.ioqr0kg.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Korean_Drama_In_Hindi_Dubbed_4u'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
